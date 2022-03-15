4c79f4e51583701e12821711245f5cc98e805a7b
imjoongyeon <dlawnddus@gmail.com>
2022-03-15 10:16:10 +0900

    db_cofig.js설정변경

--- server/config/config.js
+++ server/config/config.js
@@ -1,23 +1,25 @@
-require('dotenv').config();
-{
-  "development"= {
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  development: {
     username: "root",
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     host: "127.0.0.1",
     dialect: "mysql"
   },
-  "test" = {
+  test: {
     username: "root",
-    password: null,
-    database: "database_test",
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     host: "127.0.0.1",
     dialect: "mysql"
   },
-  "production" = {
+  production: {
     username: "root",
-    password: null,
-    database: "database_production",
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     host: "127.0.0.1",
     dialect: "mysql"
   }
