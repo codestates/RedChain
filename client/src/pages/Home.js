4fe1a7135fc0fda7d990b7ad17a533d5cbb65cea
dannsrud <dannsrud@gmail.com>
2022-03-15 09:11:34 +0900

    [front] main page 구현완료 but 아이콘 부분 수정필요

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,72 @@
-import React from "react";
+import {React, useEffect, useState} from "react";
+import axios from "axios"
+import love from "../assets/love.png"
+import coin from "../assets/dollar.png"
+import doc from "../assets/love-letter.png"
+
 
 function Home() {
+  const [userList, setUserList] = useState('');
+  const [price, setPrice] = useState('123');
+  const [supCount, setSupCount] = useState('456');
+  const [camCount,setCamCount] = useState('789');
+
+  // var addr = process.env.SERVER_ADDR;
+
+  useEffect(async () => {    //  정보를 어떻게 넘겨주는지 확인 필요! 통째로 주는지 or 필요한 정보만 주는지...
+    // /total api 적용.
+    try {
+      axios.get("http://localhost:4000" + "/total")
+      .then(res => {
+        setUserList(res.data);
+      })
+    } catch (err) {
+      console.log(err);
+    }
+  },[]);
+
+  
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div id="home">
+      <div id="home_title">
+        <h2>Red Chain에 닿은 사랑의 손길</h2>
+        <div>여러분의 사랑으로 세상이 따뜻해지고 있습니다.</div>
+      </div>
+      <div id="home_support">
+        <div className="home__support">
+          <div className="home__support__title">
+            <img src={coin}/>
+            <div>후원 금액</div>
+          </div>
+          <div className="home__support__contents">
+            <span>{price}</span>
+            <em>KLAY</em>
+            </div>
+        </div>
+        <div className="home__support">
+          <div className="home__support__title">
+            <img src={love} />
+            <div>후원 횟수</div>
+          </div>
+          <div className="home__support__contents">
+            <span>{supCount}</span>
+            <em>건</em>
+            </div>
+        </div>
+        <div className="home__support">
+          <div className="home__support__title">
+            <img src={doc}/>
+            <div>캠페인 수</div>
+          </div>
+          <div className="home__support__contents">
+            <span>{camCount}</span> 
+            <em>건</em>
+          </div>
+        </div>
+      </div>
     </div>
+
   );
 }
 
--- client/src/pages/SupportNFT.js
+++ client/src/pages/SupportNFT.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 function SupportNFT() {
+  // 1. 내가 가지고 있는 nft 출력.
+  // 2. 여기서 '기부'를 누르면 nft 경매페이지에 해당 nft가 보이고 여기서는 더 이상 안보이게 한다.
   return(
     <div>
       <h1>SupportNFT</h1>
