c4a7dfe498ecfbbd731f2331835cd9f6deb961de
imjoongyeon <dlawnddus@gmail.com>
2022-03-31 22:17:51 +0900

    auction최신화

--- client/src/pages/NftAuctionView.js
+++ client/src/pages/NftAuctionView.js
@@ -1,131 +1,105 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
-import { getAccount } from "../Klaytn/KIP17";
+import { getAccount, addDays } from "../Klaytn/util";
 import BidTable from "../components/bidTable";
 import "../styles/NftAuctionView.css";
+import axios from 'axios';
+import Caver from 'caver-js';
+import AuctionABI from '../Klaytn/AuctionABI'
 
-import dummyNFT from "../components/dummyNFT";
-import dummyBidding from "../components/dummyBidding";
 
 
-// NFT에 들어가 있는 정보 : img, 기부한 날짜, 현재 가격
-// NFT에 X, 보여줄 수 있는 정보 : 판매종료 날짜(기부날짜 + 2 week), 
-// 확인사항??!! 옥션에 응시했던사람들 목록?, 기부자 내용?
-
-function NftAuctionView() {
-  const [auctionInfo, setAuctionInfo] = useState([]);
-  const [biddigInfo, setBiddigInfo] = useState([]);
-  const [biddingPrice,setbiddingPrice] = useState(0);
-  const [count, setCount] = useState(0);
-
+function NftAuctionView({auctionList}) {
   const id = useParams().id;
-  const colNames = ['응찰자', '응찰가격', '응찰날짜'];
 
-  const [day, setDay] =useState(-1);
-  const [hour, setHour] =useState(-1);
-  const [min, setMin] =useState(-1);
-  const [sec, setSec] =useState(-1);
+  const [auctionInfo, setAuctionInfo] = useState({
+    startAt:auctionList[id-1].startAt,
+    endAt:auctionList[id-1].endAt,
+    highestBid:auctionList[id-1].highestBid,
+    tokenURI:auctionList[id-1].tokenURI,
+    auctionAddress:auctionList[id-1].auctionAddress,
+  });
+  const [orderbook, setOrderbook] = useState([]);
+  const [biddingPrice,setbiddingPrice] = useState(0);
 
-  const times = new Date(auctionInfo.end_at);
-  const today = new Date();
-  const diff = times - today;
-
-  // console.log(`time : ${times} today : ${today}`)
-  
-  useEffect (async()=> {  
-    setAuctionInfo(dummyNFT[id-1]);  // dummy
-    setBiddigInfo(dummyBidding);     // dummy
-
-    getAuction();
-    getbiddig();
-    
-    
-  },[]);
-
-  useEffect (() => {  //count 1s에 한번씩 남은시간 업로드.
-    setTimeout(() => { setCount(count + 1) }, 1000); // 1s
-
-    setDay( String(Math.floor(diff / (1000*60*60*24))) );
-    setHour( String(Math.floor((diff / (1000*60*60)) % 24)).padStart(2,"0") );
-    setMin( String(Math.floor((diff / (1000*60)) % 60)).padStart(2,"0") );
-    setSec( String(Math.floor(diff / 1000 % 60)).padStart(2,"0") );
-  },[count])
-
-  
-  const getAuction = () => {  // tokenId가 id 인 NFT 호출해서 auctionInfo에 저장.
-    // await axios.get(`http://localhost:4000/nft/auction/${id}`)
-    // .then((res)=> {})
-    // .catch((err) => console.log(err));
-  }
-
-  const getbiddig = () => {  // 해당 NFT 게시글에 맞는 응찰내역 호출해서 biddigInfo에 저장
-
-  }
+ 
+    const getOrderbook = async() => {  // 응찰내역
+      await axios.get(`http://localhost:4000/auction/detail/${id}`)
+      .then((res)=> {
+        if(res.data === null) {
+          setOrderbook(0);
+        } else {
+          console.log(res.data);
+          setOrderbook(res.data);
+        }
+      })
+      .catch((err) => console.log(err));
+    }
 
 
   const bidding = async() => {  // 응찰 함수
-    // 1. 나의 주소가 필요. 나의 응찰희망가격이 필요.
-    // 2. 디비에 해당 tokenId에 맞는 것에주소랑 응찰가격이랑 넣기. --> 최고 응찰가보다 작으면 거절.
+    if(auctionInfo.highestBid < biddingPrice) {
+      let now;
+      const account = await getAccount();  // account[0] 에 내 주소값 들어가있음.
+      const caver = new Caver(window.klaytn);
+      const AuctionContract = new caver.klay.Contract(AuctionABI, auctionInfo.auctionAddress);
+      await AuctionContract.methods.bid().send({
+        from: account[0],
+        value: caver.utils.toPeb(biddingPrice,'KLAY'),
+        gas: 100000,
+      }).then(async(tx) => {
 
-    if(auctionInfo.bid_price < biddingPrice) {
-      const account = await getAccount();  // account 에 내 주소값 들어가있음.
+        if(tx) {
+          now = addDays(new Date(), 0);
+          await axios.post(`http://localhost:4000/orderbook/${id}`, {
+            bidder: account[0],
+            bid:biddingPrice,
+            createdAt: now,
+          })
+          .then((res)=> {
+              console.log(res.data);
+              const newOrder = {
+                auctionId:id,
+                bidder: account[0],
+                bid:biddingPrice,
+                createdAt: now,
+              }
+              setOrderbook([...orderbook, newOrder]);
+              setAuctionInfo({
+                startAt: auctionInfo.startAt,
+                endAt: auctionInfo.endAt,
+                tokenURI: auctionInfo.tokenURI,
+                auctionAddress: auctionInfo.auctionAddress,
+                highestBid: biddingPrice,
+              })
+          })
+          .catch(err => console.log(err));
+        }
 
-
-      // DB에 tokenId에 맞는 NFT에 응찰한 주소랑 응찰가격 을 저장하는 API
-      // API 성공하면 자동으로 새로고침해서 최고 응찰가 업글 하기.
-      
-      
-    } else {
-      alert(`현재 최고 응찰가보다 높게 응찰해 주세요.`)
+      })
     }
-    
+    else {
+      alert(`현재 최고 응찰가보다 높게 응찰해 주세요.`)
+    }  
   }
 
-  
-  
-  //  img donor end_at bid_price
+  useEffect (()=> {
+    getOrderbook();
+
+    },[]);
+
   return(
     <div className="view">
       <div className="view__img">  {/* NFT 이미지 */}
-        <img src={auctionInfo.img} alt="" />
+        <img src={auctionInfo.tokenURI} alt="토큰URI" />
       </div>
       <div className="view__contents"> {/* NFT 관련 내용. */}
-        <div className="view__donated" >
-          <span>donated by </span>
-          <div>{auctionInfo.donor}</div>
-        </div>
-
         <div className="view__in">
           <div className="view__info">
-            {/* <div className="view__startAt">시작날짜 : {auctionInfo.create_at}</div> */}
-            <div className="view__endtAt">
-              <div>Sale ends {auctionInfo.end_at}</div>
-              <div className="view__check"> {/* 남은시간 체크 */}
-                <div className="view__timebox">  
-                  <div>{day}</div>
-                  <p>Days</p>
-                </div>
-                <div className="view__timebox">  
-                  <div>{hour}</div>
-                  <p>Hours</p>
-                </div>
-                <div className="view__timebox">  
-                  <div>{min}</div>
-                  <p>Minutes</p>
-                </div>
-                <div className="view__timebox">  
-                  <div>{sec}</div>
-                  <p>Seconds</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="view__info">
-            <div >
-              <div>Highest bidding price </div>
-              <span className="view__price">{auctionInfo.bid_price} </span>
-              <span> KLAY</span>
-            </div>
+            <div className="view__startAt">시작날짜 : {auctionInfo.startAt}</div>
+            <div className="view__endtAt">종료날짜 : {auctionInfo.endAt}</div>
+            <div className="view__price">최고 응찰가 : {auctionInfo.highestBid}</div>
+            
           </div>
           <div className="view__input">
             <input type="number" value={biddingPrice} onChange={(e)=>{setbiddingPrice(e.target.value)}}/>
@@ -133,7 +107,7 @@ function NftAuctionView() {
           </div>
         </div>
         <div className="view__bidHistoy">
-          <BidTable list={biddigInfo} colNames={colNames}/>
+          <BidTable list={orderbook} />
         </div>
       </div>
     </div>
--- client/src/pages/NftSeal.js
+++ client/src/pages/NftSeal.js
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from "react";
 import "../styles/SupportNFT.css"
 import Card from "../components/NFT_card";
 import dummyNFT from "../components/dummyNFT";
+import randomboxImg from "../assets/randombox.gif"
+import axios from "axios";
+import Caver from 'caver-js';
+import SealABI from "../Klaytn/SealABI";
+import season1 from '../assets/randombox1/season1';
 
 function NftSeal() {
   // 기부 받은 nft 전체 목록 출력.
@@ -10,25 +15,60 @@ function NftSeal() {
   const [DonationNFTList, setDonationNFTList] = useState([]);  // nft seal 리스트
 
   // Seal NFT 목록 DonationNFTList 불러오는 APInst DonationNFTList = dummyNFT;
+  const BuyButton = async() => {
+    const contractAddress = "0x4491c6193C54CCF990cbb47E2900372Ca32F16cf";
+    await window.klaytn.enable();
+    const caver = new Caver(window.klaytn)
+    const sealContract = new caver.klay.Contract(SealABI, contractAddress);
+    await sealContract.methods.buy().send({
+      from: window.klaytn.selectedAddress,
+      gas: '50000',
+      value: caver.utils.toPeb(1,'KLAY'),
+      gasPrice: null,
+    }).then(async(tx) => {
+      //0xde0b6b3a7640000 = 10 ** 18 = 1klay
+      if(tx.value = "0xde0b6b3a7640000") {
+        await axios.post('http://localhost:4000/buy/seal', {
+          // transactionHash: tx.transactionHash,
+          account : window.klaytn.selectedAddress,
+          tokenId : parseInt(tx.events[0].raw.data),
+        }).then((res)=> console.log(res));
+      }
+    }).catch(err => console.log(err));
+  }
+
   useEffect (async()=> {  
-    setDonationNFTList(dummyNFT);
+    setDonationNFTList(season1);
     
   },[]);
 
   return(
     <div id ="supNFT">
       <div id="supNFT-title">
-        <h2>Random Box</h2>
-        <div>이곳의 수익은 후원금액으로 사용됩니다.</div>
+        <h2>CRYPTO Seal</h2>
+        <div>기부 받은 NFT 경매페이지이며, 이곳의 수익은 후원금액으로 사용됩니다.</div>
       </div>
       <div id="supNFT-contents">
         { DonationNFTList === null ? 
           <h1>No NFT to display</h1>
           :
           DonationNFTList.map((NFT,idx) => {
-            return <Card NFT={NFT} idx={idx} key={idx} onErrorImg={NFT.img} text={"/nft/seal/"}/> 
+            return <Card NFT={NFT} idx={idx} key={idx} img={NFT.img} quantity={NFT.quantity} text={"/nft/seal/"}/> 
           })
         }
+      </div>
+      <div className = "randombox"> 
+          <h1>RandomBox</h1>
+          <div className="randombox-box">
+            <div className="randombox-img">
+              <img src={randomboxImg}></img>
+            </div>
+            <div>
+              30 Klay
+            </div>
+            <button onClick={BuyButton}>구매하기</button>
+          </div>
+
       </div>
     </div>
   );
