01d6335dc21f0571d8c0ec94eaaa1cd86fe19c44
imjoongyeon <dlawnddus@naver.com>
2022-04-21 14:51:00 +0900

    aws 서버설정변경

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const cors =require('cors');
 const app = express();
 const port = 4000;
-
+require(dotenv).config();
+const MYDNS = process.env.PUBLICDNS;
 const indexRoute = require('./routes');
 
 app.use(cors({
-    origin: "http://localhost:3000",
-    creadentials:true,
+    origin: ["http://localhost:3000", MYDNS],
+    credentials:true,
     }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
